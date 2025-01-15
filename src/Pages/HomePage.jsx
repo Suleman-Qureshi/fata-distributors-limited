941ea6d0a604641e8a23173316fdad25a59c9f5b
Suleman Qureshi <143796869+Suleman-Qureshi@users.noreply.github.com>
2025-01-16 04:55:56 +0500

    slider update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,11 +3,11 @@ import Slider from '../Components/Slider';
 function HomePage() {
   return (
     <>
-    <section className='w-screen h-screen bg-[url("herobgHome.gif")] bg-cover bg-no-repeat bg-top relative before:absolute before:w-full before:h-full bg-red-600'>
+    <section className='w-screen h-screen bg-[url("herobgHome.gif")] bg-cover bg-no-repeat bg-top relative before:absolute before:w-full before:h-full bg-red-600 overflow-y-hidden'>
       {/* <div className='h-screen flex justify-end mr-12'>
       <img src="./png 3.png" alt="" className='h-full' />
       </div> */}
-   <Slider imgSrc1='https://images.unsplash.com/photo-1736644772468-aacc3d6bde24?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D' imgSrc2='https://images.unsplash.com/photo-1736606355698-5efdb410fe93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D' />
+   <Slider imgSrc1='./product_1.png' imgSrc2='https://images.unsplash.com/photo-1736606355698-5efdb410fe93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D' />
     </section>
     </>
   )
